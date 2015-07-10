837f207a73fb52ba90d1272c66d8d7daee3edae6
Aditya B <ab@oorjalabs.com>
2015-07-10 22:59:18 +0100

    ver 0.0.6.1
      1. Update js to activate all functionality
    
      2. Merge css
    
      3. Clean up options-new.html
    
      4. Clean up options.css
    
      5. Clean up options.js

--- js/options.js
+++ js/options.js
@@ -4,6 +4,12 @@ var LOGOUT_CONFIRM = chrome.i18n.getMessage("logout_confirm");
 var IDT_URL = "https://iDoneThis.com/home/"
 
 $(document).ready(function(){
+  var appName = chrome.i18n.getMessage("appName");
+  var shortName = chrome.i18n.getMessage("shortName");
+  $("#extShortName").text(shortName);
+  $("#extName").text(appName);
+  $("title").text("Options - " + appName);
+  
   bgPage.iDoneThis.isLoggedIn(function(){
     updateOptionsPage(true);
   }, function(){
@@ -11,7 +17,8 @@ $(document).ready(function(){
   });
   
   // login event handler
-  $("#connect").on("click", function(){
+  $("#connect").on("click", function(e){
+    e.preventDefault();
     console.log("init Login");
     bgPage.iDoneThis.connect(function(){
       updateOptionsPage(true);
@@ -35,6 +42,7 @@ $(document).ready(function(){
   
   // refreshTeams event handler
   $("#refreshTeams").on("click", function(e){
+    e.preventDefault();
     bgPage.iDoneThis.getTeams(updateTeams);
   });
   
@@ -50,9 +58,7 @@ $(document).ready(function(){
     if(idtToken !== undefined && idtToken !== localStorage.idtToken){
       localStorage.idtToken = idtToken;
       $("#idtTokenHelp").fadeOut("fast", function(){
-        $("#idtTokenSaved").fadeIn("fast", function(){
-          $("#idtTokenSaved").delay(5000).fadeOut("fast");
-        });
+        confirmSave(this.id);
       });
     } else {
       $("#idtTokenHelp").fadeOut("fast");
@@ -79,7 +85,33 @@ $(document).ready(function(){
     
     // Update team name on options page to reflect new default team.
     $("#defaultTeam").delay(250).text(localStorage.defaultTeam).attr("href", localStorage.defaultTeamURL);
-    // $(this).blur();
+    confirmSave(this.id);
+  });
+  
+  loadHelpMessages();
+  positionHelps();
+  
+  $("#cx-edit-help-content div").hover(function(){
+    $(this).css("display", "inherit");
+  },function(){
+    $(this).css("display", "none");
+  });
+
+  $("section").hover(function(){
+    $("#"+$(this).attr("id")+"Help").css("display", "inherit");
+  },function(){
+    $("#"+$(this).attr("id")+"Help").css("display", "none");
+  });
+
+  $("section *").focusin(function(){
+    $("#"+$(this).parents("section").attr("id")+"Help").css("display", "inherit");
+  });
+  $("section *").focusout(function(){
+    $("#"+$(this).parents("section").attr("id")+"Help").css("display", "none");
+  });
+
+  $(document).resize(function(){
+    positionHelps();
   });
   
 });
@@ -107,14 +139,14 @@ function updateOptionsPage(loggedIn){
     $("#idtTokenInput").removeAttr("disabled");
     if(localStorage.idtToken && localStorage.idtToken !== ""){
       $("#idtTokenInput").val(localStorage.idtToken);
-      // $("#idtTokenErrorListItem").fadeOut();
       $("#connect").removeAttr("disabled");
     } else {
       $("#idtTokenInput").focus();
-      // $("#idtTokenErrorListItem").fadeIn();
       $("#connect").attr("disabled", "disabled");
     }
-    // $("#loginErrorListItem").fadeIn();
+    
+    $("#teamSelect").text("");
+    $("#teamSelect, #refreshTeams").attr("disabled", "disabled");
     
     bgPage.setupExtensionState(false);
   }
@@ -122,34 +154,51 @@ function updateOptionsPage(loggedIn){
 
 
 function updateTeams(callback){
-  if(localStorage.teamCount > 1){
-    chrome.storage.local.get("teams", function(st){
-      if(st && st.teams && st.teams.length > 0){
-        var teamSelect = $("#teamSelect");
-        var o;
-        
-        teamSelect.html("");
-        
-        for (var i = 0; i < st.teams.length; i++) {
-          o = new Option(st.teams[i].name, JSON.stringify({
-            name: st.teams[i].name,
-            short_name: st.teams[i].short_name,
-            permalink: st.teams[i].permalink
-          }));
-          if(st.teams[i].short_name === localStorage.defaultTeamCode) o.selected = true;
-          teamSelect.append(o);
-        }
-        
-        if(st.teams.length > 1)
-          $("#teams").css("display", "inherit");
+  chrome.storage.local.get("teams", function(st){
+    if(st && st.teams && st.teams.length > 0){
+      var teamSelect = $("#teamSelect");
+      var o;
+      
+      teamSelect.html("");
+      
+      for (var i = 0; i < st.teams.length; i++) {
+        o = new Option(st.teams[i].name, JSON.stringify({
+          name: st.teams[i].name,
+          short_name: st.teams[i].short_name,
+          permalink: st.teams[i].permalink
+        }));
+        if(st.teams[i].short_name === localStorage.defaultTeamCode) o.selected = true;
+        teamSelect.append(o);
       }
-      
-      $("#defaultTeam").text(localStorage.defaultTeam).attr("href", localStorage.defaultTeamURL);
-      
-      if(callback) callback();
-    });
-  } else {
+    }
+    
     $("#defaultTeam").text(localStorage.defaultTeam).attr("href", localStorage.defaultTeamURL);
-    $("#teams").css("display", "none");
-  }
+    
+    if(callback) callback();
+  });
+  
+  $("#teamSelect, #refreshTeams").removeAttr("disabled");
 }
+
+
+function loadHelpMessages(){
+  $("#cx-edit-help-content div").each(function(){
+    $("#"+this.id).html(chrome.i18n.getMessage(this.id));
+  });
+}
+
+function positionHelps(){
+  $("section").each(function(){
+    var id = $(this).attr("id");
+    $("#"+id+"Help").css({
+      // top: $(this).position().top-53,
+      top: $(this).position().top-53+$(this).height()/2-$("#"+id+"Help").height()/2
+    });
+  });
+}
+
+function confirmSave(source){
+  console.log("Option saved: " + source);
+  $("#"+source+"Saved").delay(400).fadeIn("fast").delay(2000).fadeOut("fast");
+}
+
