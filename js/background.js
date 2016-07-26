02ee6a43f120f91ca34e6ddb1b8b5d0b8610408b
Aditya B <ab@oorjalabs.com>
2016-07-27 00:10:49 +0100

    ver 1.0.3.7
      1. UPDATE: Changed on and off badge background colours to improve contrast with text
    
      2. UPDATE: Moved constants in background js to constants.js

--- js/background.js
+++ js/background.js
@@ -46,23 +46,6 @@ var dateFormattingStrings = [
   ]
 ];
   
-var UNINSTALL_URL = "http://c306.net/whygo.html?src=qdt";
-var DONE_FOR_ANDROID_URL = "https://play.google.com/store/apps/details?id=net.c306.done&utm_source=DoneForChromeNotification&utm_medium=notification&utm_campaign=DoneForChrome";
-var NOTIFICATION_ICON_URL = chrome.extension.getURL("img/done-128.png");
-var DONE_FOR_ANDROID_NOTIFICATION_ICON_URL = chrome.extension.getURL("img/dfapromo-icon-128.png");
-var BUTTON_GREEN = "#33ff33";
-var BUTTON_GREY = "#999999";
-var BUTTON_RED = "#ff3333";
-var TEAM_CHECKER_ALARM = "teamChecker";
-var CONNECTION_CHECKER_ALARM = "connectionChecker";
-var DONE_CHECKER_ALARM = "doneChecker";
-var OFFLINE_SYNC_ALARM = "offlineSyncer";
-var OFFLINE_SYNC_FREQUENCY = 15;
-var DAILY_NOTIFICATION_ALARM = "dailyNotificationAlarm";
-var DAILY_NOTIFICATION_ID = "dailyNotification";
-var DONE_FOR_ANDROID_NOTIFICATION_ID = "doneForAndroidNotification";
-var NEW_DONE_NOTIFICATION = "newDoneNotification";
-
 localStorage.doneFrequency = localStorage.doneFrequency || 15;
 localStorage.dailyNotification = localStorage.dailyNotification || "true";
 localStorage.dailyNotificationTime = localStorage.dailyNotificationTime || "19:00";
---
+++ js/constants.js
@@ -0,0 +1,17 @@
+var UNINSTALL_URL = "http://c306.net/whygo.html?src=qdt";
+var DONE_FOR_ANDROID_URL = "https://play.google.com/store/apps/details?id=net.c306.done&utm_source=DoneForChromeNotification&utm_medium=notification&utm_campaign=DoneForChrome";
+var NOTIFICATION_ICON_URL = chrome.extension.getURL("img/done-128.png");
+var DONE_FOR_ANDROID_NOTIFICATION_ICON_URL = chrome.extension.getURL("img/dfapromo-icon-128.png");
+var BUTTON_GREY = "#999999";
+var BUTTON_RED = [255,0,0,180];
+var BUTTON_GREEN = [0,255,0,150];
+var TEAM_CHECKER_ALARM = "teamChecker";
+var CONNECTION_CHECKER_ALARM = "connectionChecker";
+var DONE_CHECKER_ALARM = "doneChecker";
+var OFFLINE_SYNC_ALARM = "offlineSyncer";
+var OFFLINE_SYNC_FREQUENCY = 15;
+var DAILY_NOTIFICATION_ALARM = "dailyNotificationAlarm";
+var DAILY_NOTIFICATION_ID = "dailyNotification";
+var DONE_FOR_ANDROID_NOTIFICATION_ID = "doneForAndroidNotification";
+var NEW_DONE_NOTIFICATION = "newDoneNotification";
+
